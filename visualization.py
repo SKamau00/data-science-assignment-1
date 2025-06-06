9b13486e26edcae6380dc3cdd11922e4ad215cb7
SKamau00 <sammiekamau27@gmail.com>
2025-06-06 12:43:26 +0300

    Add files via upload

---
+++ visualization.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+months = ['07/2019', '08/2019', '09/2019', '10/2019', '11/2019']
+searches = [50, 53, 59, 56, 62]
+direct = [39, 47, 42, 51, 51]
+social_media = [70, 80, 90, 87, 92]
+
+x = np.arange(len(months))  
+width = 0.25  
+
+
+plt.figure(figsize=(10, 6))
+plt.bar(x - width, searches, width, label='Searches', color='deepskyblue')
+plt.bar(x, direct, width, label='Direct', color='lightcoral')
+plt.bar(x + width, social_media, width, label='Social Media', color='orange')
+
+plt.xlabel('Months')
+plt.ylabel('Visitors (in thousands)')
+plt.title('Visitors by web traffic sources')
+plt.xticks(x, months)
+plt.legend()
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+
+plt.savefig("web_traffic_chart.png")  
+plt.tight_layout()
+plt.show()
